233b5de115cf112c5b297fd4ead8dfabd9a7d192
Joao Vitor P. Moraes <jvlppm@gmail.com>
2014-10-17 00:56:07 -0300

    Exposed Game Context on Xamarin.Forms.

--- src/Xamarin.Forms.Platforms.Xna/Forms.cs
+++ src/Xamarin.Forms.Platforms.Xna/Forms.cs
@@ -2,6 +2,7 @@
 {
     using Microsoft.Xna.Framework;
     using Xamarin.Forms.Platforms.Xna;
+    using Xamarin.Forms.Platforms.Xna.Context;
 
     public static class Forms
     {
@@ -10,12 +11,21 @@
         public static Game Game { get; private set; }
         public static IPlatformEngine PlatformEngine { get; private set; }
 
+#if !INTERNAL_CONTEXT
+        public static GameContext DrawContext { get; private set; }
+        public static GameContext UpdateContext { get; private set; }
+#endif
+
         public static void Init(Game game)
         {
             if (IsInitialized)
                 return;
 
             PlatformServices platformServices = new PlatformServices(game);
+#if !INTERNAL_CONTEXT
+            DrawContext = platformServices.DrawContext;
+            UpdateContext = platformServices.UpdateContext;
+#endif
 
             game.Components.Add(platformServices);
             Device.PlatformServices = platformServices;
